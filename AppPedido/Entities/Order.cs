c51c70dfdd21cec75cfa2737025d2d715cfb73bd
Marcelo Dos Santos <marcelo_mdss@live.com>
2022-01-15 00:05:12 -0300

    Utilizando Enumeração e composição

---
+++ AppPedido/Entities/Order.cs
@@ -0,0 +1,64 @@
+﻿using AppPedido.Entities.Enums;
+using System.Text;
+using System.Collections.Generic;
+using System;
+using System.Globalization;
+
+namespace AppPedido.Entities
+{
+    public class Order 
+    {
+        public DateTime Moment = DateTime.Now;
+
+        public OrderStatus Status { get; set; }
+
+        public Client Client { get; set; }
+
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Order(OrderStatus status, Client client)
+        {
+            
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+        public double Total()
+        {
+            double sum = 0.0;
+            foreach (OrderItem item in Items)
+            {
+                sum += item.SubTotal();
+            }
+            return sum;
+         }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("ORDER SUMMARY");
+            sb.AppendLine("Order moment: " + Moment.ToString("dd/MM/yyyy"));
+            sb.AppendLine("Order status: " + Status);
+            sb.AppendLine("Client: " + Client);
+            sb.AppendLine("Order items: ");
+            
+
+            foreach(OrderItem item in Items)
+            {
+                sb.AppendLine(item.ToString());
+            }
+            sb.AppendLine("Total price: $" + Total().ToString("F2", CultureInfo.InvariantCulture));
+            return sb.ToString();
+
+        }
+
+    }
+
+}   
---
+++ AppPedido/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using AppPedido.Entities;
+using AppPedido.Entities.Enums;
+using System.Globalization;
+
+
+namespace AppPedido
+
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Console.WriteLine("Enter cliente data:");
+            Console.Write("Nome: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime barthdate = DateTime.Parse(Console.ReadLine());
+
+            Client client = new Client(name, email, barthdate);
+
+            
+            Console.WriteLine();
+
+            Console.WriteLine("Enter order data:");
+
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+
+            Order order = new Order(status, client);
+
+            Console.WriteLine();
+
+            Console.WriteLine("How many items to this order?");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Enter #{i} item data:");
+
+
+                Console.Write("Product name: ");
+                string ProductName = Console.ReadLine();
+
+                Console.Write("Product price: ");
+                double ProductPrice = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Product product = new Product(ProductName, ProductPrice );
+
+                Console.Write("Quantity: ");
+                int quantity = int.Parse(Console.ReadLine());
+                OrderItem item = new OrderItem(quantity, product, ProductPrice);
+
+                order.AddItem(item);
+            }
+
+            Console.WriteLine(order);
+
+
+            
+        }
+    }
+}
